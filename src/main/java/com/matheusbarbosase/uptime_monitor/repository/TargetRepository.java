ea400466280e4b712543903057e594b5c9339b22
Matheus Barbosa <matheusalolbarbosa@gmail.com>
2025-10-15 14:53:02 -0300

    feat(repository): define TargetRepository interface extending JpaRepository

--- src/main/java/com/matheusbarbosase/uptime_monitor/repository/TargetRepository.java
+++ src/main/java/com/matheusbarbosase/uptime_monitor/repository/TargetRepository.java
@@ -1,4 +1,10 @@
 package com.matheusbarbosase.uptime_monitor.repository;
 
-public interface TargetRepository {
+import com.matheusbarbosase.uptime_monitor.model.Target;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface TargetRepository extends JpaRepository<Target, Long> {
 }
