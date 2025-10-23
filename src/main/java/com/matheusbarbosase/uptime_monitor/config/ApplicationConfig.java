97047e1a7cc8e9c08cbc6f4b5954c01d14bec8ea
Matheus Barbosa <matheusalolbarbosa@gmail.com>
2025-10-23 17:01:24 -0300

    feat(security): configure application beans for authentication (UserDetailsService, PasswordEncoder)

---
+++ src/main/java/com/matheusbarbosase/uptime_monitor/config/ApplicationConfig.java
@@ -0,0 +1,41 @@
+package com.matheusbarbosase.uptime_monitor.config;
+
+import com.matheusbarbosase.uptime_monitor.repository.UserRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    public ApplicationConfig(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(passwordEncoder());
+
+        authProvider.setUserDetailsService(userDetailsService());
+
+        return authProvider;
+    }
+}
