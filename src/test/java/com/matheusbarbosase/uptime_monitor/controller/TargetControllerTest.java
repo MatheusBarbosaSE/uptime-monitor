739931b62eecb8d530ccf3756517c95c9c264f8a
Matheus Barbosa <matheusalolbarbosa@gmail.com>
2025-11-11 17:10:22 -0300

    feat(test): add test for authenticated user access to targets

--- src/test/java/com/matheusbarbosase/uptime_monitor/controller/TargetControllerTest.java
+++ src/test/java/com/matheusbarbosase/uptime_monitor/controller/TargetControllerTest.java
@@ -1,13 +1,21 @@
 package com.matheusbarbosase.uptime_monitor.controller;
 
+import com.matheusbarbosase.uptime_monitor.service.TargetService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Collections;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -15,6 +23,9 @@ class TargetControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private TargetService targetService;
 
     @Test
     void whenGetTargetsAnonymously_thenShouldReturn403Forbidden() throws Exception {
@@ -25,5 +36,19 @@ class TargetControllerTest {
                 .andExpect(
                         status().isForbidden()
                 );
+    }
+
+    @Test
+    @WithMockUser(username = "test-user")
+    void whenGetTargetsAsAuthenticatedUser_thenShouldReturnOkAndEmptyList() throws Exception {
+
+        when(targetService.findAllTargets())
+                .thenReturn(Collections.emptyList());
+
+        mockMvc.perform(
+                        get("/api/targets")
+                )
+                .andExpect(status().isOk())
+                .andExpect(content().json("[]"));
     }
 }
